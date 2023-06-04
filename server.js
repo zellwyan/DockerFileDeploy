489c07c8aad2bab5de31715b5b6c04b80aa2c1bc
zellwyan <zellwyan@students.zhaw.ch>
2023-06-04 16:35:27 +0200

    Ausgabe verändert

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)');
+res.send('Hello All, Welcome to the world of Docker!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
